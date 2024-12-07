6742aa8c640bc021f15465f5e3da74e843d48098
Ivanoel17 <143780462+Ivanoel17@users.noreply.github.com>
2024-12-07 17:30:07 +0700

    Update server.js versi hello world

--- src/server/server.js
+++ src/server/server.js
@@ -1,36 +1,43 @@
 require('dotenv').config();
 
-
 const Hapi = require('@hapi/hapi');
 const routes = require('../server/routes');
 const loadModel = require('../services/loadModel');
 const InputError = require('../exceptions/InputError');
 
-
-
 (async () => {
     const server = Hapi.server({
-        port: 3000,
-        host: '0.0.0.0',
+        port: 8080,
+        host: '127.0.0.1',
         routes: {
             cors: {
               origin: ['*'],
             },
         },
-    })
+    });
 
     const model = await loadModel();
     server.app.model = model;
 
-    server.route(routes); 
+    server.route(routes);
+
+    // Tambahkan route baru di sini
+    server.route({
+        method: 'GET',
+        path: '/',
+        handler: (request, h) => {
+            return { message: 'Hello World!' };
+        }
+    });
+
     server.ext('onPreResponse', function (request, h) {
         const response = request.response;
         if (response instanceof InputError) {
             const newResponse = h.response({
                 status: 'fail',
                 message: `${response.message}`
-            })
-            newResponse.code(response.statusCode)
+            });
+            newResponse.code(response.statusCode);
             return newResponse;
         }
         if (response.isBoom) {
@@ -43,7 +50,7 @@ const InputError = require('../exceptions/InputError');
         }
         return h.continue;
     });
- 
+
     await server.start();
     console.log(`Server start at: ${server.info.uri}`);
 })();
