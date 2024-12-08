dd9218e9f37eb6ac6f964a1097920f197e711656
Ivanoel17 <143780462+Ivanoel17@users.noreply.github.com>
2024-12-08 13:40:39 +0700

    Update handler.js get rid of the unpredicted

--- src/server/handler.js
+++ src/server/handler.js
@@ -21,14 +21,11 @@ async function postPredictHandler(request, h) {
 
   await storeData(id, data);
 
-  const response = h.response({
-    status: "success",
-    message:
-      confidenceScore > 99
-        ? "Model is predicted successfully"
-        : "Model is predicted successfully but under threshold. Please use the correct picture",
-    data,
-  });
+    const response = h.response({
+        status: 'success',
+        message: 'Model is predicted successfully',
+        data
+    });
   response.code(201);
   return response;
 }
