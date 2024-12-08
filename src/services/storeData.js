238172a8f78b942e0a6698b3e65965e25b679f8a
Ivanoel17 <143780462+Ivanoel17@users.noreply.github.com>
2024-12-08 12:13:49 +0700

    Update storeData.js without the json

--- src/services/storeData.js
+++ src/services/storeData.js
@@ -1,21 +1,10 @@
 const { Firestore } = require('@google-cloud/firestore');
-const path = require('path');
 
-const pathKey = path.resolve('./submissionmlgc-ivanoel-rhema-63a2769325a8.json')
-     
 async function storeData(id, data) {
+    const db = new Firestore();
 
-  try {
-    const db = new Firestore({
-      projectId: 'submissionmlgc-ivanoel-rhema',
-      keyFilename: pathKey,
-    });
- 
-    const predictCollection = db.collection('predictions');
-    return predictCollection.doc(id).set(data);
-  }catch(error) {
-    console.error(error);
-  }
+    const preditCollection = db.collection('predictions');
+    return preditCollection.doc(id).set(data);
 }
- 
+
 module.exports = storeData;
