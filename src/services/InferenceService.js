75c7d292109be645a8058e3ddab3344832dc97e0
Ivanoel17 <143780462+Ivanoel17@users.noreply.github.com>
2024-12-08 13:36:40 +0700

    Update InferenceService.js dokter!

--- src/services/InferenceService.js
+++ src/services/InferenceService.js
@@ -24,7 +24,7 @@ async function predictClassification(model, image) {
 
     if (label === "Cancer") {
       suggestion =
-        "Segera periksa ke dokter";
+        "Segera periksa ke dokter!";
     } else {
       suggestion =
         "Penyakit kanker tidak terdeteksi.";
